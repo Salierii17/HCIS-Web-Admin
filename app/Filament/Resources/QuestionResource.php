c04c10cc282a9968f6cfe04e1cd7a8eb208af3cf
mifthahulwirma <mifthahulwirma21@gmail.com>
2025-05-19 18:18:46 +0700

    nambahin package

--- app/Filament/Resources/QuestionResource.php
+++ app/Filament/Resources/QuestionResource.php
@@ -23,7 +23,7 @@ class QuestionResource extends Resource
 
     protected static ?string $navigationGroup = 'Training';
 
-    protected static ?int $navigationSort = -2;
+    protected static ?int $navigationSort = -3;
      public static function form(Form $form): Form
     {
         return $form
