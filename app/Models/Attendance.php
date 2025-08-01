98756a11c79c279b420c6af828ba3336e21e3202
EMIYAID04 <sulthanza06@gmail.com>
2025-08-01 13:04:36 +0700

    fix(attendance): resolve merge conflicts in attendance model

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -6,8 +6,6 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Attendance extends Model
 {
