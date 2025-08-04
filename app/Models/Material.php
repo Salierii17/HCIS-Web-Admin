272d3bf6c5489e9c66fbda76bdf5af08a32343c6
mifthahulwirma <mifthahulwirma21@gmail.com>
2025-08-05 01:11:37 +0700

    Update Employee Website Access

--- app/Models/Material.php
+++ app/Models/Material.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Material extends Model
 {
@@ -13,4 +14,9 @@ class Material extends Model
         'nama_material',
         'file_path',
     ];
+
+    public function assignedTrainings()
+    {
+        return $this->hasMany(AssignTraining::class);
+    }
 }
---
+++ app/Policies/MaterialPolicy.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Material;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class MaterialPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->can('view_any_material');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Material $material): bool
+    {
+        return $user->can('view_material');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->can('create_material');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Material $material): bool
+    {
+        return $user->can('update_material');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Material $material): bool
+    {
+        return $user->can('delete_material');
+    }
+
+    /**
+     * Determine whether the user can delete any models.
+     */
+    public function deleteAny(User $user): bool
+    {
+        return $user->can('delete_any_material');
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Material $material): bool
+    {
+        return $user->can('restore_material');
+    }
+
+    /**
+     * Determine whether the user can restore any models.
+     */
+    public function restoreAny(User $user): bool
+    {
+        return $user->can('restore_any_material');
+    }
+
+    /**
+     * Determine whether the user can replicate the model.
+     */
+    public function replicate(User $user, Material $material): bool
+    {
+        return $user->can('replicate_material');
+    }
+
+    /**
+     * Determine whether the user can reorder the models.
+     */
+    public function reorder(User $user): bool
+    {
+        return $user->can('reorder_material');
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Material $material): bool
+    {
+        return $user->can('force_delete_material');
+    }
+
+    /**
+     * Determine whether the user can permanently delete any models.
+     */
+    public function forceDeleteAny(User $user): bool
+    {
+        return $user->can('force_delete_any_material');
+    }
+}
